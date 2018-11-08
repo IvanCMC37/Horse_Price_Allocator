ca5d5c51f10478f3cfa7334535e55ece3af43d66
Ivan Chan <s3534199@student.rmit.edu.au>
2018-11-09 08:16:37 +1100

    created test cases

--- dotnet-code-challenge/DataProcessor/IDataLoader.cs
+++ dotnet-code-challenge/DataProcessor/IDataLoader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace dotnet_code_challenge.Utilities
+namespace dotnet_code_challenge.DataProcessor
 {
     public interface IDataLoader
     {
